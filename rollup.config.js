d6e44c0f68d92b5b8c5edc4cf496110695e881c3
reslene <reslene@numary.com>
2022-07-28 12:59:42 +0200

    fix(): try to fix react hook issue (2 reacts instances) by removing some plugin from rollup
    Signed-off-by: reslene <reslene@numary.com>

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
-import external from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require('./package.json');
 
@@ -25,12 +23,10 @@ export default [
       },
     ],
     plugins: [
-      external(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       terser(),
-      peerDepsExternal(),
     ],
     external: [
       'react',
