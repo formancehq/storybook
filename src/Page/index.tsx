e3edf3cb9c0b111073be39b3580353d9d8d742a5
reslene <reslene@numary.com>
2022-08-01 18:04:55 +0200

    feat(Page): add dumb component page
    Signed-off-by: reslene <reslene@numary.com>

---
+++ src/Page/index.tsx
@@ -0,0 +1,77 @@
+import { Clear } from '@mui/icons-material';
+import { Box, Typography } from '@mui/material';
+import React, { FunctionComponent, ReactElement, ReactNode } from 'react';
+import { SxProps } from '@mui/system';
+import { Theme } from '@mui/material/styles';
+import { LoadingButton } from '../LoadingButton';
+
+export type PageProps = {
+  children: ReactElement;
+  id: string;
+  title?: string | ReactNode;
+  subtitle?: string;
+  onClick?: () => void;
+  actionLabel?: string;
+  actionEvent?: string;
+  actionId?: string;
+  sx?: SxProps<Theme>;
+};
+
+export const Page: FunctionComponent<PageProps> = ({
+  children,
+  id,
+  onClick,
+  title,
+  subtitle,
+  actionLabel,
+  actionEvent,
+  actionId,
+  sx = {},
+}) => {
+  const renderTitle = () => {
+    if (typeof title === 'string') {
+      return (
+        <Typography variant="h1" mr={1}>
+          {title}
+        </Typography>
+      );
+    }
+
+    return title;
+  };
+
+  return (
+    <Box id={id} pt={7} pb={7} pr={20} pl={20} sx={{ ...sx }}>
+      <Box
+        pl={3}
+        pr={3}
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box display="flex">
+          {renderTitle()}
+          {subtitle && (
+            <Typography variant="h2" color="primary" mr={1}>
+              {subtitle}
+            </Typography>
+          )}
+        </Box>
+        <Box>
+          {onClick && actionLabel && actionEvent && (
+            <LoadingButton
+              startIcon={<Clear style={{ transform: 'rotate(-45deg)' }} />}
+              onClick={onClick}
+              content={actionLabel}
+              variant="dark"
+              id={actionId}
+            />
+          )}
+        </Box>
+      </Box>
+      <Box pl={3} pr={3} mt={4} mb={4}>
+        {children}
+      </Box>
+    </Box>
+  );
+};
