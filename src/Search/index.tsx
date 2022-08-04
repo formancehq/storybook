bdf1e97c6e401e81889fae13bfbdb20d8bba1d75
reslene <reslene@numary.com>
2022-08-04 14:39:00 +0200

    feat(Search): add suggestion box
    Signed-off-by: reslene <reslene@numary.com>

--- src/Search/index.tsx
+++ src/Search/index.tsx
@@ -1,62 +1,154 @@
-import React, { FunctionComponent } from 'react';
-import { Box, Modal, TextField } from '@mui/material';
+import React, { FunctionComponent, ReactElement, useState } from 'react';
+import { Box, Modal, TextField, Typography } from '@mui/material';
 import { SearchOutlined } from '@mui/icons-material';
 import { CFunction } from '../types';
+import { LoadingButton } from '../LoadingButton';
+
+export type Suggestion = {
+  label: string;
+  icon: ReactElement;
+  more: {
+    action: () => void;
+    label: string;
+  };
+  total: number;
+  items: {
+    id: number;
+    label: string;
+    onClick: (id: number | string) => void;
+  }[];
+};
 
 export type SearchProps = {
   open: boolean;
   onClose: () => void;
   onChange: CFunction<any>;
+  children?: ReactElement;
+  suggestions: Suggestion[];
 };
 
 export const Search: FunctionComponent<SearchProps> = ({
   onClose,
   open,
   onChange,
-}) => (
-  <Modal
-    open={open}
-    onClose={onClose}
-    aria-labelledby="modal-modal-title"
-    aria-describedby="modal-modal-description"
-  >
-    <Box
-      sx={{
-        position: 'absolute' as const,
-        top: '30%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 600,
-        height: '50px',
-        margin: 0,
-      }}
+  suggestions,
+}) => {
+  const [searchValue, setSearchValue] = useState(undefined);
+
+  const handleOnChange = (event: any) => {
+    setSearchValue(event.target.value);
+    onChange(event);
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
     >
-      <TextField
-        placeholder={'Search'}
-        name="terms"
-        required={true}
-        fullWidth
-        onChange={onChange}
-        autoComplete="off"
-        InputProps={{
-          sx: {
-            '& svg': { color: ({ palette }) => palette.neutral[400] },
-            backgroundColor: ({ palette }) => palette.neutral[0],
-            '& fieldset': {
-              border: 'none',
-            },
-          },
-          startAdornment: (
-            <SearchOutlined
-              color="primary"
-              sx={{
-                marginRight: 1,
-                color: ({ palette }) => palette.neutral[400],
-              }}
-            />
-          ),
+      <Box
+        sx={{
+          position: 'absolute' as const,
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 600,
+          margin: 0,
         }}
-      />
-    </Box>
-  </Modal>
-);
+      >
+        <TextField
+          placeholder={'Search'}
+          name="terms"
+          required={true}
+          fullWidth
+          onChange={handleOnChange}
+          autoComplete="off"
+          InputProps={{
+            sx: {
+              height: 56,
+              '& svg': { color: ({ palette }) => palette.neutral[400] },
+              backgroundColor: ({ palette }) => palette.neutral[0],
+              '& fieldset': {
+                border: 'none',
+              },
+            },
+            startAdornment: (
+              <SearchOutlined
+                color="primary"
+                sx={{
+                  marginRight: 1,
+                  color: ({ palette }) => palette.neutral[400],
+                }}
+              />
+            ),
+          }}
+        />
+        <Box
+          sx={{
+            borderTop: ({ palette }) => `1px solid ${palette.neutral[100]}`,
+          }}
+        />
+        {searchValue && (
+          <Box>
+            {suggestions.map((suggestion, index) => (
+              <Box
+                key={index}
+                p={2}
+                sx={{
+                  backgroundColor: ({ palette }) => palette.neutral[0],
+                  borderBottom: ({ palette }) =>
+                    `1px solid ${palette.neutral[100]}`,
+                }}
+              >
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Box
+                    display="flex"
+                    justifyContent="flex-start"
+                    alignItems="center"
+                  >
+                    <Typography
+                      ml={1}
+                      sx={{ color: ({ palette }) => palette.neutral[600] }}
+                    >
+                      {suggestion.label}
+                    </Typography>
+                  </Box>
+                  <LoadingButton
+                    variant="dark"
+                    content={suggestion.more.label}
+                    onClick={suggestion.more.action}
+                  />
+                </Box>
+                {suggestions &&
+                  suggestions.length > 0 &&
+                  suggestion.items.map((item) => (
+                    <Box key={item.id}>
+                      <LoadingButton
+                        startIcon={suggestion.icon}
+                        content={item.label}
+                        onClick={() => item.onClick(item.id)}
+                      />
+                    </Box>
+                  ))}
+                <Box display="flex" justifyContent="end" mr={2}>
+                  <Typography
+                    ml={1}
+                    variant="footNote"
+                    sx={{ color: ({ palette }) => palette.neutral[400] }}
+                  >
+                    {suggestion.total} results
+                  </Typography>
+                </Box>
+              </Box>
+            ))}
+          </Box>
+        )}
+      </Box>
+    </Modal>
+  );
+};
--- src/Search/stories.tsx
+++ src/Search/stories.tsx
@@ -1,21 +1,96 @@
-import React from 'react';
-import { Search } from './index';
+import React, { useState } from 'react';
+import { Search, Suggestion } from './index';
 import { LoadingButton } from '../LoadingButton';
-import { SearchOutlined } from '@mui/icons-material';
+import {
+  DoneAll,
+  DriveFileRenameOutline,
+  EditLocation,
+  SearchOutlined,
+} from '@mui/icons-material';
 import { Box } from '@mui/material';
+import { noop } from '../utils';
 
 export default {
   title: 'Design System/Search/Spotlight',
   component: Search,
 };
 
+const suggestions: Suggestion[] = [
+  {
+    label: 'Payment',
+    icon: <DriveFileRenameOutline />,
+    more: { action: () => console.log('go to payments'), label: 'More' },
+    total: 140,
+    items: [
+      {
+        id: 1,
+        label: 'payin:679472',
+        onClick: (id: number | string) => {
+          console.log('click', id);
+        },
+      },
+      {
+        id: 2,
+        label: 'payout:679473',
+        onClick: (id: number | string) => {
+          console.log('click', id);
+        },
+      },
+    ],
+  },
+  {
+    label: 'Transaction',
+    icon: <DoneAll />,
+    more: { action: () => console.log('go to transaction'), label: 'More' },
+    total: 14,
+    items: [
+      {
+        id: 1,
+        label: '000679472',
+        onClick: (id: number | string) => {
+          console.log('click', id);
+        },
+      },
+      {
+        id: 2,
+        label: '000679473',
+        onClick: (id: number | string) => {
+          console.log('click', id);
+        },
+      },
+    ],
+  },
+  {
+    label: 'Accounts',
+    icon: <EditLocation />,
+    more: { action: () => console.log('go to accounts'), label: 'More' },
+    total: 180,
+
+    items: [
+      {
+        id: 1,
+        label: '000679472',
+        onClick: (id: number | string) => {
+          console.log('click', id);
+        },
+      },
+      {
+        id: 2,
+        label: '000679473',
+        onClick: (id: number | string) => {
+          console.log('click', id);
+        },
+      },
+    ],
+  },
+];
+
 export const DefaultSpotlight = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const handleOnChange = (event: any) => {
-    console.log(event);
-  };
+
+  const handleOnChange = () => noop();
 
   return (
     <Box>
@@ -24,7 +99,12 @@ export const DefaultSpotlight = () => {
         onClick={handleOpen}
         variant="dark"
       />
-      <Search open={open} onChange={handleOnChange} onClose={handleClose} />
+      <Search
+        open={open}
+        onChange={handleOnChange}
+        onClose={handleClose}
+        suggestions={suggestions}
+      />
     </Box>
   );
 };
