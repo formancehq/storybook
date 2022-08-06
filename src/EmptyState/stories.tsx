19a4ed985b50f32b2ac91b80e3d393622e4011e7
reslene <reslene@numary.com>
2022-08-06 03:21:17 +0200

    feat(EmptyState): add children + dark variant
    Signed-off-by: reslene <reslene@numary.com>

--- src/EmptyState/stories.tsx
+++ src/EmptyState/stories.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import { EmptyState } from './index';
+import { LoadingButton } from '../LoadingButton';
+import { noop } from '../utils';
+import { Box } from '@mui/material';
 
 export default {
   title: 'Design System/Containers/EmptyState',
   component: EmptyState,
 };
 
-export const DefaultEmptyState = () => (
+export const LighttEmptyState = () => (
   <EmptyState
     title="Choose a ledger to get started"
     description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
   />
 );
-DefaultEmptyState.storyName = 'Default';
+LighttEmptyState.storyName = 'Light';
+
+export const DarkEmptyState = () => (
+  <EmptyState
+    variant="dark"
+    title="Choose a ledger to get started"
+    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  />
+);
+DarkEmptyState.storyName = 'Dark';
+
+export const DarkWithChildrenEmptyState = () => (
+  <EmptyState
+    variant="dark"
+    title="Choose a ledger to get started"
+    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  >
+    <Box mt={3}>
+      <LoadingButton
+        content="Choose my ledger"
+        variant="primary"
+        onClick={() => noop()}
+      />
+    </Box>
+  </EmptyState>
+);
+DarkWithChildrenEmptyState.storyName = 'Dark with children';
