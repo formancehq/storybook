a58d1de2b3ab660c0ba843247866c020e75af6a8
reslene <reslene@numary.com>
2022-08-12 16:56:10 +0200

    feat(JsonTextArea): create component + stories
    Signed-off-by: reslene <reslene@numary.com>

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-export * from './Sections/Txid';
 export * from './Buttons/LoadingButton';
 export * from './Buttons/TabButton';
 export * from './Buttons/SelectButton';
-export * from './Containers/Page';
+
 export * from './Sections/Amount';
 export * from './Sections/Date';
 export * from './Sections/SourceDestination';
+export * from './Sections/Txid';
+
 export * from './Containers/EmptyState';
-export * from './Search';
+export * from './Containers/Page';
 export * from './Containers/SectionWrapper';
+
+export * from './Forms/TextFields';
+export * from './Forms/JsonTextArea';
+
 export * from './Dataviz/StatsCard';
+export * from './Search';
 export * from './Breadcrumbs';
 export * from './Modal';
-export * from './Forms/TextFields';
+
 export * from './utils';
 export * from './muiTheme';
