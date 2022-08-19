a151d4874dcdb402e4df2627c1a3adf1cbfcc007
reslene <reslene@numary.com>
2022-08-19 17:36:11 +0200

    feat(SourceDestination): make ellipse mandatory
    Signed-off-by: reslene <reslene@numary.com>

--- src/Sections/SourceDestination/index.tsx
+++ src/Sections/SourceDestination/index.tsx
@@ -3,19 +3,21 @@ import React, { FunctionComponent } from 'react';
 
 export type SourceDestinationProps = {
   label: string;
+  ellipse: boolean;
   onClick?: () => void;
   sx?: SxProps<Theme>;
 };
 
 export const SourceDestination: FunctionComponent<SourceDestinationProps> = ({
   label,
+  ellipse = true,
   onClick,
   sx,
 }) => {
   if (!label) return null;
   const length = label.length;
   const name =
-    length > 20
+    ellipse && length > 20
       ? `${label.substring(0, Math.floor((length * 15) / 100))}...:${
           label.split(':')[1]
         }`
