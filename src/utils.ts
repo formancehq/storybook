b2fd8be7af780c7c63cb92174dcb84b46945f8c7
Sofiane <sofiane@formance.com>
2022-09-02 15:08:48 +0200

    feat(Utils): Add get method

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const noop: any = (_item?: any) => undefined;
 
+export const get = <T, K>(
+  value: T,
+  getter: (t: T) => K,
+  defaultValue: K | null = null
+) => {
+  try {
+    return getter(value) ?? defaultValue;
+  } catch {
+    return defaultValue;
+  }
+};
+
 export const storyDocsParameters = {
   docs: {
     source: {
