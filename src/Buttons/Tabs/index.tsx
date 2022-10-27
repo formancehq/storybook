c0b32b387c0ff2c39e4e3ebe67b6c7cd480e4570
reslene <reslene@numary.com>
2022-10-27 10:20:30 +0200

    feat(Tabs): add action button
    Signed-off-by: reslene <reslene@numary.com>

--- src/Buttons/Tabs/index.tsx
+++ src/Buttons/Tabs/index.tsx
@@ -2,20 +2,33 @@ import React, { FunctionComponent } from 'react';
 
 import { Box } from '@mui/material';
 
+import { LoadingButton, LoadingButtonProps } from '../LoadingButton';
 import { TabButton, TabButtonProps } from '../TabButton';
 
-export type TabsProps = { config: TabButtonProps[] };
+export type TabsProps = {
+  config: TabButtonProps[];
+  action?: LoadingButtonProps;
+};
 
-export const Tabs: FunctionComponent<TabsProps> = ({ config }) => (
-  <Box sx={{ display: 'flex', gap: '26px' }}>
-    {config.map((tab: TabButtonProps, index: number) => (
-      <TabButton
-        key={index}
-        type={tab.type}
-        label={tab.label}
-        onClick={tab.onClick}
-        active={tab.active}
-      />
-    ))}
+export const Tabs: FunctionComponent<TabsProps> = ({ config, action }) => (
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: action ? 'space-between' : 'start',
+      alignItems: 'center',
+    }}
+  >
+    <Box sx={{ display: 'flex', gap: '26px' }}>
+      {config.map((tab: TabButtonProps, index: number) => (
+        <TabButton
+          key={index}
+          type={tab.type}
+          label={tab.label}
+          onClick={tab.onClick}
+          active={tab.active}
+        />
+      ))}
+    </Box>
+    {action && <LoadingButton {...action} />}
   </Box>
 );
--- src/Buttons/Tabs/stories.tsx
+++ src/Buttons/Tabs/stories.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { Add } from '@mui/icons-material';
+
 import { storyDocsParameters } from '../../utils';
 
 import { Tabs } from './index';
@@ -34,3 +36,34 @@ export const DefaultTabs = () => {
 };
 DefaultTabs.storyName = 'Active tabs';
 DefaultTabs.parameters = storyDocsParameters;
+
+export const TabsWithAction = () => {
+  const [actives, setActives] = useState<{
+    transaction: boolean;
+    account: boolean;
+  }>({ transaction: true, account: false });
+
+  const config = [
+    {
+      type: 'transaction',
+      label: 'Transaction',
+      onClick: () => setActives({ transaction: true, account: false }),
+      active: actives.transaction,
+    },
+    {
+      type: 'account',
+      label: 'Account',
+      onClick: () => setActives({ transaction: false, account: true }),
+      active: actives.account,
+    },
+  ];
+
+  return (
+    <Tabs
+      config={config}
+      action={{ content: 'Action', startIcon: <Add />, variant: 'dark' }}
+    />
+  );
+};
+TabsWithAction.storyName = 'Active tabs with action';
+TabsWithAction.parameters = storyDocsParameters;
