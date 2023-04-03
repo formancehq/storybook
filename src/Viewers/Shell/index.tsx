736f52d502130e8e57ac96ae42614f540c32792a
reslene <reslene@numary.com>
2023-04-03 18:24:38 +0200

    feat(Shell): make mandatory some props + add children props
    Signed-off-by: reslene <reslene@numary.com>

--- src/Viewers/Shell/index.tsx
+++ src/Viewers/Shell/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, ReactElement } from 'react';
 
 import { ContentCopy } from '@mui/icons-material';
 import { Box, Typography } from '@mui/material';
@@ -7,29 +7,47 @@ import { LoadingButton } from '../../Buttons/LoadingButton';
 import { copyTokenToClipboard } from '../../utils';
 
 export type ShellViewerProps = {
-  data: string;
+  data?: string;
+  copy?: boolean;
+  children?: ReactElement;
 };
 
-export const ShellViewer: FunctionComponent<ShellViewerProps> = ({ data }) => (
+export const ShellViewer: FunctionComponent<ShellViewerProps> = ({
+  data,
+  copy = true,
+  children,
+}) => (
   <Box
     sx={{
       background: (theme) => theme.palette.neutral[900],
       color: (theme) => theme.palette.default.bright,
       padding: '16.5px 14px',
       minHeight: '200px',
-      display: 'flex',
       borderRadius: '6px',
-      justifyContent: 'space-between',
-      gap: '12px',
-      alignSelf: 'center',
     }}
   >
-    <Typography variant="money">{data}</Typography>
-    <LoadingButton
-      id="shell-copy"
-      variant="dark"
-      onClick={() => copyTokenToClipboard(data)}
-      startIcon={<ContentCopy />}
-    />
+    <>
+      {data && (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            gap: '12px',
+            alignSelf: 'center',
+          }}
+        >
+          <Typography variant="money">{data}</Typography>
+          {copy && (
+            <LoadingButton
+              id="shell-copy"
+              variant="dark"
+              onClick={() => copyTokenToClipboard(data)}
+              startIcon={<ContentCopy />}
+            />
+          )}
+        </Box>
+      )}
+      {children && children}
+    </>
   </Box>
 );
--- src/Viewers/Shell/stories.tsx
+++ src/Viewers/Shell/stories.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { Box, List, ListItem, Typography } from '@mui/material';
+
 import { LoadingButton } from '../../Buttons/LoadingButton';
 import { Modal } from '../../Modal';
 import { storyDocsParameters } from '../../utils';
@@ -35,3 +37,31 @@ export const ShellModal = () => {
 
 ShellModal.storyName = 'Modal';
 ShellModal.parameters = storyDocsParameters;
+
+export const ShellWithChildren = () => (
+  <ShellViewer>
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Typography variant="money">
+        Send 14690 EUR/2 from account world to account
+        payment:e90f61a4_37b0_4878_9903_bbaabb1e16cd (ledger
+        orchestration-000-internal)
+      </Typography>
+      <Typography ml={1} variant="money" color="secondary">
+        Created transaction: 0
+      </Typography>
+      <Typography ml={1} variant="money" color="secondary">
+        Reference: payment:e90f61a4_37b0_4878_9903_bbaabb1e16cd
+      </Typography>
+    </Box>
+  </ShellViewer>
+);
+
+ShellWithChildren.storyName = 'Children';
+ShellWithChildren.parameters = storyDocsParameters;
+
+export const ShellWithoutButton = () => (
+  <ShellViewer data="exception error id undefined" copy={false} />
+);
+
+ShellWithoutButton.storyName = 'Without copy';
+ShellWithoutButton.parameters = storyDocsParameters;
