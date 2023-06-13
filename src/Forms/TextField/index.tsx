f00d7cbf638e92716c4dcd271892081ee3b7d1c0
Wiliam Traoré <hi@wil-liam.com>
2023-06-13 11:56:55 +0200

    refactor(text-field): change design

--- src/Forms/TextField/index.tsx
+++ src/Forms/TextField/index.tsx
@@ -22,10 +22,54 @@ export const TextField: FunctionComponent<TextFieldProps> = ({
       mt: 1,
       mb: 1,
       ...props.sx,
+      '& label': {
+        position: 'static',
+        transform: 'none',
+        fontSize: '12px',
+        mb: 0.5,
+      },
+      '& input': {
+        borderRadius: '4px',
+        borderWidth: '1px',
+        borderColor: ({ palette }) => palette.neutral[100],
+        borderStyle: 'solid',
+        padding: '0 12px',
+        height: '40px',
+        transition: 'all 0.2s ease-in-out',
+
+        '&:hover': {
+          borderColor: ({ palette }) => palette.neutral[500],
+        },
+
+        '&:focus': {
+          borderColor: ({ palette }) => palette.neutral[800],
+        },
+      },
       '& fieldset': {
         borderRadius: '6px',
         borderWidth: '1px',
         borderColor: ({ palette }) => palette.neutral[200],
+        display: 'none',
+      },
+      // Modify error styles
+      '& .Mui-error': {
+        '& input': {
+          borderColor: ({ palette }) => palette.red.normal,
+        },
+      },
+      '& .MuiFormHelperText-root': {
+        mt: 0.5,
+        mx: 0,
+      },
+      // Modify disabled styles
+      '& .Mui-disabled': {
+        '& input': {
+          backgroundColor: ({ palette }) => palette.neutral[0],
+
+          '&:hover': {
+            borderColor: ({ palette }) => palette.neutral[100],
+          },
+        },
       },
     }}
     {...props}
