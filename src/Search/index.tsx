3caf6f048197920da2a0ec72ea2be187e44e3f1c
Wiliam Traoré <hi@wil-liam.com>
2023-08-01 15:24:28 +0200

    feat(search-panel): create component

--- src/Search/index.tsx
+++ src/Search/index.tsx
@@ -1,98 +1,173 @@
-import React, { FunctionComponent, ReactElement, useState } from 'react';
+import React, {
+  CSSProperties,
+  FunctionComponent,
+  useEffect,
+  useState,
+} from 'react';
 
-import { SearchOutlined } from '@mui/icons-material';
-import {
-  Box,
-  ClickAwayListener,
-  TextField,
-  TextFieldProps,
-} from '@mui/material';
-import { isEmpty } from 'lodash';
+import { Box, Typography, useTheme, CircularProgress } from '@mui/material';
+import { Command } from 'cmdk';
+import './style.css';
 
-export type SearchProps = TextFieldProps & {
-  renderChildren: (value: string, onClose: () => void) => ReactElement;
+export type SearchGroup = {
+  title: string;
+  items: React.ReactNode[];
+  showMore?: React.ReactNode;
 };
 
-export const Search: FunctionComponent<SearchProps> = ({
-  onChange,
-  onKeyDown,
-  renderChildren,
-  ...props
-}) => {
-  const [searchValue, setSearchValue] = useState<string>();
-  const [openDropdown, setOpenDropdown] = useState<boolean>(false);
+export type SearchProps = {
+  placeholder?: string;
+  navigation?: React.ReactNode;
+  elements: SearchGroup[];
+  noResults?: string;
+  trigger: React.ReactNode;
+  isLoading?: boolean;
+  onValueChange?: (value: string) => void;
+};
 
-  const resetSearch = () => {
-    setSearchValue(undefined);
-    setOpenDropdown(false);
+export const Search: FunctionComponent<SearchProps> = (props: SearchProps) => {
+  const {
+    placeholder = 'Search an element',
+    navigation,
+    elements,
+    noResults = 'No results found',
+    trigger,
+    isLoading = false,
+    onValueChange,
+  } = props;
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [value, setValue] = useState<string>('');
+
+  const { palette } = useTheme();
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener('keydown', down);
+
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  return (
+    <>
+      <span onClick={() => setOpen(true)}>{trigger}</span>
+      <Command.Dialog open={open} onOpenChange={setOpen}>
+        <Command.Input
+          placeholder={placeholder}
+          value={value}
+          onValueChange={(value) => {
+            setValue(value);
+            onValueChange && onValueChange(value);
+          }}
+        />
+
+        {navigation && value === '' && navigation}
+
+        <Command.List
+          className={`${
+            value === '' ? 'cmdk-list-no-value' : 'cmdk-list-value'
+          } ${isLoading ? 'cmdk-list-loading' : ''}`}
+          style={{ padding: '16px', width: '100%', boxSizing: 'border-box' }}
+        >
+          {isLoading && (
+            <Command.Loading>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  width: '100%',
+                  color: palette.neutral[300],
+                }}
+              >
+                <CircularProgress sx={{ color: palette.neutral[700] }} />
+              </Box>
+            </Command.Loading>
+          )}
+
+          {!isLoading && (
+            <>
+              <Command.Empty>
+                <Typography>{noResults}</Typography>
+              </Command.Empty>
+              {elements.map((group) => (
+                <Command.Group key={group.title}>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                      mb: '8px',
+                    }}
+                  >
+                    <Typography sx={{ color: palette.neutral[300] }}>
+                      {group.title}
+                    </Typography>
+                    {group.showMore && <>{group.showMore}</>}
+                  </Box>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: '8px',
+                    }}
+                  >
+                    {group.items.map((item) => (
+                      <Command.Item key={item?.toString()}>{item}</Command.Item>
+                    ))}
+                  </Box>
+                </Command.Group>
+              ))}
+            </>
+          )}
+        </Command.List>
+      </Command.Dialog>
+    </>
+  );
+};
+
+export type SearchTriggerProps = {
+  children: React.ReactNode;
+  command?: string;
+};
+
+export const SearchTrigger = (props: SearchTriggerProps) => {
+  const { children, command = '⌘K' } = props;
+
+  const { palette } = useTheme();
+
+  const buttonStyle: CSSProperties = {
+    background: palette.neutral[700],
+    color: palette.neutral[300],
+    display: 'flex',
+    gap: '8px',
+    alignItems: 'center',
+    borderRadius: '6px',
+    border: `1px solid ${palette.neutral[500]}`,
+    width: '100%',
+    height: '40px',
+    padding: '0 8px 0 12px',
+    justifyContent: 'space-between',
   };
 
-  const handleOnChange = (event: any) => {
-    const value = event.target.value;
-    if (isEmpty(value)) {
-      resetSearch();
-    }
-    if (value && !isEmpty(value)) {
-      setSearchValue(value);
-    }
-    onChange && onChange(event);
-  };
-
-  const handleOnKeyDown = (event: any) => {
-    if (event.keyCode === 13) {
-      setOpenDropdown(true);
-    }
-    if (event.keyCode === 27) {
-      resetSearch();
-    }
-    onKeyDown && onKeyDown(event);
+  const shortcutStyle: CSSProperties = {
+    display: 'inline-flex',
+    padding: '4px 8px',
+    border: `1px solid ${palette.neutral[500]}`,
+    borderRadius: '4px',
+    fontSize: '12px',
+    background: palette.neutral[700],
   };
 
   return (
-    <ClickAwayListener onClickAway={() => setOpenDropdown(false)}>
-      <Box width={400}>
-        <TextField
-          onChange={handleOnChange}
-          autoComplete="off"
-          onKeyDown={handleOnKeyDown}
-          InputProps={{
-            sx: {
-              '& ::placeholder': {
-                color: ({ palette }) => `${palette.neutral[300]} !important`,
-                opacity: 1 /* Firefox */,
-              },
-              color: ({ palette }) => `${palette.neutral[100]} !important`,
-              borderRadius: '6px',
-              '& fieldset': {
-                borderColor: ({ palette }) =>
-                  `${palette.neutral[700]} !important`,
-              },
-              '&:hover fieldset': {
-                borderColor: ({ palette }) =>
-                  `${palette.neutral[500]} !important`,
-              },
-              '&.Mui-focused fieldset': {
-                borderColor: ({ palette }) =>
-                  `${palette.neutral[700]} !important`,
-              },
-              background: ({ palette }) => palette.neutral[700],
-            },
-            startAdornment: (
-              <SearchOutlined
-                color="primary"
-                sx={{
-                  marginRight: 1,
-                  color: ({ palette }) => palette.neutral[400],
-                }}
-              />
-            ),
-          }}
-          {...props}
-        />
-        {openDropdown &&
-          searchValue &&
-          renderChildren(searchValue, () => setOpenDropdown(false))}
-      </Box>
-    </ClickAwayListener>
+    <button style={buttonStyle}>
+      <Typography>{children}</Typography>
+      <span style={shortcutStyle}>{command}</span>
+    </button>
   );
 };
