41f853772fb5b24dc4d0090299acaa205ad9dc8a
Wiliam Traoré <hi@wil-liam.com>
2023-09-11 15:17:39 +0200

    refactor(modal): change animation & style

--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { CSSProperties, FunctionComponent } from 'react';
 
 import { Close } from '@mui/icons-material';
 import {
@@ -10,6 +10,7 @@ import {
   DialogTitle,
   useTheme,
 } from '@mui/material';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { LoadingButton } from '../Buttons/LoadingButton';
 import { ButtonVariants, CFunction } from '../types';
@@ -26,6 +27,35 @@ export type ModalProps = DialogProps & {
   };
 };
 
+const modalAnimation = {
+  initial: {
+    y: 30,
+    x: 0,
+    opacity: 0,
+  },
+  in: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    rotate: 0,
+    transition: {
+      duration: 0.3,
+      ease: 'easeInOut',
+      stiffness: 100,
+    },
+  },
+  out: {
+    x: 0,
+    y: 10,
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+      stiffness: 100,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 export const Modal: FunctionComponent<ModalProps> = ({
   id,
   open,
@@ -38,57 +68,89 @@ export const Modal: FunctionComponent<ModalProps> = ({
   const { typography, palette } = useTheme();
   const paperSx = PaperProps ? PaperProps.sx : {};
 
+  const modalContainerStyle: CSSProperties = {
+    backgroundColor: palette.common.white,
+    borderRadius: '8px',
+    boxShadow: '0 25px 50px -12px rgb(0 0 0 / 0.25)',
+  };
+
   return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      id={id}
-      PaperProps={{ sx: { minWidth: '300px', ...paperSx } }}
-    >
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <DialogTitle sx={{ ...typography.headline }}>{title}</DialogTitle>
-        <LoadingButton
-          startIcon={<Close />}
-          variant="transparent"
+    <AnimatePresence>
+      {open && (
+        <Dialog
+          open={open}
+          onClose={onClose}
+          id={id}
           sx={{
-            ':hover': {
-              background: 'transparent',
-              color: palette.neutral[600],
+            ...paperSx,
+            minWidth: '300px',
+            overflow: 'visible',
+
+            '& .MuiPaper-root': {
+              backgroundColor: 'transparent',
+              border: 'none',
+              boxShadow: 'none',
             },
           }}
-          onClick={onClose as CFunction<any>}
-        />
-      </Box>
-      <DialogContent
-        sx={{
-          overflowY: 'initial',
-          p: `0 24px ${actions ? '0' : '24px'} 24px`,
-        }}
-      >
-        {children}
-      </DialogContent>
-      {actions && (
-        <DialogActions sx={{ padding: '14px 24px' }}>
-          {actions.cancel && (
-            <LoadingButton
-              content={actions.cancel.label}
-              variant="stroke"
-              onClick={actions.cancel.onClick}
-              sx={{ width: '50%' }}
-            />
-          )}
-          {actions.save && (
-            <LoadingButton
-              type="submit"
-              content={actions.save.label}
-              variant={actions.save?.variant || 'dark'}
-              onClick={actions.save.onClick}
-              sx={{ width: '50%' }}
-              disabled={actions.save.disabled || false}
-            />
-          )}
-        </DialogActions>
+        >
+          <motion.div
+            variants={modalAnimation}
+            initial="initial"
+            animate="in"
+            exit="out"
+            style={modalContainerStyle}
+          >
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <DialogTitle sx={{ ...typography.headline }}>{title}</DialogTitle>
+              <LoadingButton
+                startIcon={<Close />}
+                variant="transparent"
+                sx={{
+                  ':hover': {
+                    background: 'transparent',
+                    color: palette.neutral[600],
+                  },
+                }}
+                onClick={onClose as CFunction<any>}
+              />
+            </Box>
+            <DialogContent
+              sx={{
+                overflowY: 'initial',
+                p: `0 24px ${actions ? '0' : '24px'} 24px`,
+              }}
+            >
+              {children}
+            </DialogContent>
+            {actions && (
+              <DialogActions sx={{ padding: '14px 24px' }}>
+                {actions.cancel && (
+                  <LoadingButton
+                    content={actions.cancel.label}
+                    variant="stroke"
+                    onClick={actions.cancel.onClick}
+                    sx={{ width: '50%' }}
+                  />
+                )}
+                {actions.save && (
+                  <LoadingButton
+                    type="submit"
+                    content={actions.save.label}
+                    variant={actions.save?.variant || 'dark'}
+                    onClick={actions.save.onClick}
+                    sx={{ width: '50%' }}
+                    disabled={actions.save.disabled || false}
+                  />
+                )}
+              </DialogActions>
+            )}
+          </motion.div>
+        </Dialog>
       )}
-    </Dialog>
+    </AnimatePresence>
   );
 };
